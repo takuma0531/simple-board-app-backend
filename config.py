62ff788a7dd32b7174df6eebcf270ac9aafb3892
正木拓馬 <tb.masaki0531@gmail.com>
2020-04-08 21:16:49 +0300

    test

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ class Config(object):
   # set up the secret key using environment variable
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-password'
   # set up where the sqlite database file is located
-  SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL')
-  
-  # don't let Flask-SQLAlchemy trackk modifications of objects and emit signals
+  SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL') or 
+  'mysql://b1878c531e499f:9272ea8a@us-cdbr-iron-east-01.cleardb.net/heroku_11984c3676ad2b9?reconnect=true'
+  # don't let Flask-SQLAlchemy track modifications of objects and emit signals
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
   
