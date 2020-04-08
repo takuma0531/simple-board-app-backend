363d6add2738b47bdca7ce69721e6ab133e9ac5e
正木拓馬 <tb.masaki0531@gmail.com>
2020-04-08 03:09:32 +0300

    deploy

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
   # set up the secret key using environment variable
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-password'
   # set up where the sqlite database file is located
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or  'sqlite:///' + os.path.join(basedir, 'posts.db')
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'posts.db')
   
   # don't let Flask-SQLAlchemy trackk modifications of objects and emit signals
   SQLALCHEMY_TRACK_MODIFICATIONS = False
