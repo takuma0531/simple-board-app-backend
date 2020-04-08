468c92e46c902bd60d8dc34bbdc118eedf218422
正木拓馬 <tb.masaki0531@gmail.com>
2020-04-08 22:33:06 +0300

    edit config.py

--- config.py
+++ config.py
@@ -9,13 +9,7 @@ class Config(object):
   # set up the secret key using environment variable
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-password'
   # set up where the sqlite database file is located
-  SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL') or 
-  'mysql+pymysql://{user}:{password}@{host}/{db}?reconnect=true'.format(**{
-    'user': os.environ.get('DATABASE_USER'),
-    'password': os.environ.get('DATABASE_PASSWORD'),
-    'host': os.environ.get('DATABASE_HOST'),
-    'db': os.environ.get('DATABASE_NAME')
-  })
+  SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'posts.db')
 
   # don't let Flask-SQLAlchemy track modifications of objects and emit signals
   SQLALCHEMY_TRACK_MODIFICATIONS = False
